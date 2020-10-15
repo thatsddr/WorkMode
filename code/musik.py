a5ddfd09356fedd5cc89cd22a729cb05c6cd676f
heilinfe <56303160+heilinfe@users.noreply.github.com>
2020-10-16 01:34:32 +0200

    commented the examample function call

--- code/musik.py
+++ code/musik.py
@@ -9,4 +9,4 @@ def webbrowsers(url_music,url_work):
         webbrowser.open_new_tab(url_work)
 
 #ecample call
-webbrowsers("https://www.youtube.com/watch?v=kg-xtouq3oc&list=RDEMB4pXSfqexQjAYE27XBWebA&start_radio=1","http://stackoverflow.com")
+#webbrowsers("https://www.youtube.com/watch?v=kg-xtouq3oc&list=RDEMB4pXSfqexQjAYE27XBWebA&start_radio=1","http://stackoverflow.com")
