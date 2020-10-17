fb866d08dd9f67e51eeaf3ce48e89e211ad77415
Domenico Di Ruocco <domenico@Domenicos-MacBook-Pro.local>
2020-10-17 20:21:40 +0200

    have a look at this

--- code/taskbarExample.py
+++ code/taskbarExample.py
@@ -1,55 +1,74 @@
 import rumps
+import sys 
+import musik
+import beepy
+from DockModule import customDock
+from BackgroundModule import Background
+from DNDModule import DND
+from ApplicationModule import Application
+from PyQt5.QtCore import Qt,QThread, pyqtSignal
 
-#example of taskbar app, the reset butto does not work for some reason
+#yes, I wanted to import this from gui but for some reason it didn't work
 
+class MyThread(QThread):
+    def open(self, path, wantToOpen):
+        self.path = path
+        self.app = Application(self.path)
+        if wantToOpen:
+            self.app.open()
+        else:
+            self.app.close()
+
+    def switchMode(self, d, bg, dndOn):
+        self.DnD = DND()
+        self.dock = customDock()
+        self.backg = Background()
+        print(bg)
+        self.backg.change(bg)
+        self.dock.removeAll()
+        self.dock.addMultiple(reversed(d))
+        self.dock.save()
+        if dndOn:
+            self.DnD.turnOn()
+        else:
+            self.DnD.turnOff()
+        
+
+    def web(self):
+        musik.webbrowsers("https://www.youtube.com/watch?v=kg-xtouq3oc&list=RDEMB4pXSfqexQjAYE27XBWebA&start_radio=1","http://stackoverflow.com")
+
+#from the taskbar icon, change the mode
 class taskBarApp(rumps.App):
     def __init__(self, name):
-        self.config = {
-            "app_name": name,
-            "break_message": "Time is up! Take a break :)",
-            "interval": 7
-        }
         super(taskBarApp, self).__init__(name)
-        self.timer = rumps.Timer(self.on_tick, 1)
-        self.interval = self.config["interval"]
-        self.start = rumps.MenuItem("Start", callback = self.startFunc)
-        self.reset = rumps.MenuItem("Reset", callback=self.resetFunc)
-        self.menu = [self.start, self.reset]
-        self.resetFunc()
+        self.config={'normalBG' : "/System/Library/Desktop Pictures/Catalina.heic",
+                    'workBG' : "/System/Library/Desktop Pictures/Mojave.heic",
+                    'normalDock' : ['/Applications/Safari.app', '/Applications/Google Chrome.app', '/Applications/Visual Studio Code.app', '/Applications/iTerm.app', '/Applications/Slack.app', '/Applications/Notion.app'],
+                    'workDock' : ['/Applications/Google Chrome.app', '/Applications/Visual Studio Code.app', '/Applications/Xcode.app', '/Applications/zoom.us.app', '/Applications/Discord.app',  '/Applications/Slack.app',  '/System/Applications/Reminders.app', '/Applications/iTerm.app', '/System/Applications/Stocks.app']}
+        self.title = "MacMode"
+        self.work = rumps.MenuItem("Work Mode", callback=self.switchMode)
+        self.menu = [self.work]
+        self.title = "🔆"
     
-    def resetFunc(self):
-        self.timer.count = 0
-        self.timer.stop()
-        self.title = "🍅"
-        self.menu['Start'].title = "Start"
-        self.reset.set_callback(None)
-
-    def startFunc(self, sender):
-        if sender.title.lower() == 'start':
-            self.timer.count = 0
-            self.timer.end = self.interval
-            self.timer.start()
-            sender.title = 'Pause'
-        elif sender.title.lower() == 'pause':
-            self.timer.stop()
-            sender.title = 'Continue'
-            self.reset.set_callback(self.resetFunc)
-        elif sender.title.lower() == 'continue':
-            self.timer.start()
-            sender.title = 'Pause'
-            self.reset.set_callback(None)
+    def start_program(self):
+        self.thread = MyThread()
+        #self.thread.web()
+        self.thread.switchMode(self.config['workDock'], self.config['workBG'], True)
+        self.thread.open('/Applications/Notion.app', True)
+        self.title = "💼"
         
-    def on_tick(self, sender):
-        time_left = sender.end - sender.count
-        mins = time_left // 60 if time_left >= 0 else time_left // 60 + 1
-        secs = time_left % 60 if time_left >= 0 else (-1 * time_left) % 60
-        if mins == 0 and time_left < 0:
-            rumps.notification(title=self.config["app_name"], subtitle=self.config["break_message"], message='')
-            self.resetFunc()
-        else:
-            '''self.reset.set_callback(None)'''
-            self.title = '{:02d}:{:02d}'.format(mins, secs)
-        sender.count += 1
+    def end_program(self):
+        self.thread = MyThread()
+        self.thread.switchMode(self.config['normalDock'], self.config['normalBG'], False)
+        self.thread.open('/Applications/Notion.app', False)
+        self.title = "🔆"
+    
+    def switchMode(self, sender):
+        if sender.state == 0:
+            self.start_program()
+        elif sender.state == 1:
+            self.end_program()
+        sender.state = not sender.state
 
 if __name__ == "__main__":
-    taskBarApp("Helo").run()
+    taskBarApp("MacMode").run()
