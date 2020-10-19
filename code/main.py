39fc9186a68d031805cdaa5420dbb2359bd9b89e
heilinfe <56303160+heilinfe@users.noreply.github.com>
2020-10-20 00:37:19 +0200

    add main.py

--- code/main.py
+++ code/main.py
@@ -3,8 +3,10 @@ import taskitem
 from multiprocessing import Process
 
 #starts the multithreading
+
 if __name__ == '__main__':
     p1 = Process(target = gui.guipy,args=())
     p1.start()
     p2 = Process(target = taskitem.taskBar,args=())
     p2.start()
+
