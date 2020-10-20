244f78345c523b26cb2a7f2c9b54c296c9d99785
heilinfe <56303160+heilinfe@users.noreply.github.com>
2020-10-20 18:10:50 +0200

    init setup file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+APP = ['code/main.py']
+DATA_FILES = ['settings.json']
+OPTIONS = {
+    'argv_emulation': True,
+    'plist': {
+        'LSUIElement': True,
+    },
+    'packages': ['rumps'],
+}
+
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
