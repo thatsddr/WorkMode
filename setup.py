6b67006d3f47c0a459f4efa9b98fe15431dcbb76
heilinfe <56303160+heilinfe@users.noreply.github.com>
2020-10-20 20:14:28 +0200

    Added more options

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
 from setuptools import setup
 
-APP = ['code/main.py']
+APP = ['code/WorkMode.py']
 DATA_FILES = ['code/settings.json']
-OPTIONS = {'argv_emulation': True, 'plist': {'LSUIElement': True,}, 'includes':['rumps', 'pyobjc', 'PyQt5','osascript','webbrowser']}
+OPTIONS = {'argv_emulation': True,
+            "iconfile": "icon.icns",
+            'plist': {
+                'LSUIElement': True,
+                'CFBundleName': "WorkMode",
+                'CFBundleDisplayName': "WorkMode",
+                'CFBundleVersion': "0.1.0",
+                'CFBundleShortVersionString': "0.1.0",
+                'NSHumanReadableCopyright': u"Copyright © 2015, Felix Heilingbrunner, Domenico Di Ruocco, All Rights Reserved"
+            },
+            'includes':['rumps', 'pyobjc', 'PyQt5', 'osascript', 'webbrowser']
+}
+            
 
 
 setup(
